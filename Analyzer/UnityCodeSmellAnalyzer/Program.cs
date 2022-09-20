540ccdd4f9d2f230e61f486cdbf3b86af3093e32
Matteo Bosco <matteobosco@alice.it>
2022-09-20 17:47:30 +0200

    analyzer command string

--- Analyzer/UnityCodeSmellAnalyzer/Program.cs
+++ Analyzer/UnityCodeSmellAnalyzer/Program.cs
@@ -16,75 +16,67 @@ namespace UnityCodeSmellAnalyzer
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+
+        protected static List<string> fileList = new List<string>();
+        protected static List<SyntaxTree> compilationUnits = new List<SyntaxTree>();
+        static readonly MetadataReference Mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+
+        public static void Main(string[] args)
         {
-            // Attempt to set the version of MSBuild.
-            var visualStudioInstances = MSBuildLocator.QueryVisualStudioInstances().ToArray();
-            var instance = visualStudioInstances.Length == 1
-                // If there is only one instance of MSBuild on this machine, set that as the one to use.
-                ? visualStudioInstances[0]
-                // Handle selecting the version of MSBuild you want to use.
-                : SelectVisualStudioInstance(visualStudioInstances);
-
-            Console.WriteLine($"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
-
-            // NOTE: Be sure to register an instance with the MSBuildLocator 
-            //       before calling MSBuildWorkspace.Create()
-            //       otherwise, MSBuildWorkspace won't MEF compose.
-            MSBuildLocator.RegisterInstance(instance);
-
-            using (var workspace = MSBuildWorkspace.Create())
+            // mandatory -d option
+            if (args.Length <= 0)
             {
-                // Print message for WorkspaceFailed event to help diagnosing project load failures.
-                workspace.WorkspaceFailed += (o, e) => Console.WriteLine(e.Diagnostic.Message);
-
-                var solutionPath = args[0];
-                Console.WriteLine($"Loading solution '{solutionPath}'");
-
-                // Attach progress reporter so we print projects as they are loaded.
-                var solution = await workspace.OpenSolutionAsync(solutionPath, new ConsoleProgressReporter());
-                Console.WriteLine($"Finished loading solution '{solutionPath}'");
-
-                // TODO: Do analysis on the projects in the loaded solution
+                Console.WriteLine("Directory path is mandatory, -d [directory/path]");
+                return;
             }
+
+            string command = args[0];
+            string directory = "";
+
+            if (command.Equals("-d")) { directory = args[1]; }
+
+            LoadFileList(directory);
+            Analyze();
         }
 
-        private static VisualStudioInstance SelectVisualStudioInstance(VisualStudioInstance[] visualStudioInstances)
-        {
-            Console.WriteLine("Multiple installs of MSBuild detected please select one:");
-            for (int i = 0; i < visualStudioInstances.Length; i++)
-            {
-                Console.WriteLine($"Instance {i + 1}");
-                Console.WriteLine($"    Name: {visualStudioInstances[i].Name}");
-                Console.WriteLine($"    Version: {visualStudioInstances[i].Version}");
-                Console.WriteLine($"    MSBuild Path: {visualStudioInstances[i].MSBuildPath}");
-            }
 
-            while (true)
+        protected static void LoadFileList(string directory)
+        {
+            try
             {
-                var userResponse = Console.ReadLine();
-                if (int.TryParse(userResponse, out int instanceNumber) &&
-                    instanceNumber > 0 &&
-                    instanceNumber <= visualStudioInstances.Length)
+                string[] dir = { ".cs" };
+                List<string> files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(f => dir.Any(f.ToLower().EndsWith)).ToList();
+                foreach (string f in files)
                 {
-                    return visualStudioInstances[instanceNumber - 1];
+                    fileList.Add(f);
                 }
-                Console.WriteLine("Input not accepted, try again.");
             }
-        }
-
-        private class ConsoleProgressReporter : IProgress<ProjectLoadProgress>
-        {
-            public void Report(ProjectLoadProgress loadProgress)
+            catch (Exception)
             {
-                var projectDisplay = Path.GetFileName(loadProgress.FilePath);
-                if (loadProgress.TargetFramework != null)
-                {
-                    projectDisplay += $" ({loadProgress.TargetFramework})";
-                }
-
-                Console.WriteLine($"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\\:ss\\.fffffff} {projectDisplay}");
+                Console.WriteLine("Directory not found!");
             }
         }
+
+        protected static void Analyze()
+        {
+            foreach (string file in fileList)
+            {
+                SyntaxTree tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file));
+                compilationUnits.Add(tree);
+            }
+
+            foreach(SyntaxTree cu in compilationUnits)
+            {
+                CompilationUnitSyntax root = cu.GetCompilationUnitRoot();
+                CSharpCompilation compilation = CSharpCompilation.Create(null, syntaxTrees: GetCU(), references: new[] { Mscorlib });
+                SemanticModel model = compilation.GetSemanticModel(cu);
+            }
+        }
+
+        protected static IEnumerable<SyntaxTree> GetCU()
+        {
+            return compilationUnits;
+        }
+
     }
 }
