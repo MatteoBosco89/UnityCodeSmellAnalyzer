c3852e80b74a0f7d1743fa915872a965831dce38
SenselessMite <pasqualexbob@outlook.com>
2022-09-29 18:46:01 +0200

    Fixed dictionary not correctly recognized

--- Analyzer/UnityDataSmellAnalyzer/Class/Element.cs
+++ Analyzer/UnityDataSmellAnalyzer/Class/Element.cs
@@ -15,7 +15,7 @@ namespace Element
         virtual public int LoadNormalDictionary(string[] lines, int i, string cmpId) { return 0; }
         virtual public void LoadParenthesisDictionary(string line) { }
         virtual public int LoadSpecialDictionary(string[] lines, int i) { return 0; }
-        virtual public int LoadDictionaryWithSpecialElements(string[] lines, int i) { return 0; }
+        virtual public int LoadDictionaryWithSpecialElements(string[] lines, int i, string cmpId) { return 0; }
 
     }
 }
