652e38dddf5eabcbda3e47378df3a7430cc60be2
SenselessMite <pasqualexbob@outlook.com>
2022-09-30 16:28:27 +0200

    Added static class InvocationSmell to find smell relative to invocationi of specific methods inside the desired method

---
+++ example/InvocationSmellFinder/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace InvocationSmellFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\pasqu\\Documents\\Università\\Tesi\\UnityCodeSmellAnalyzer\\Analyzer\\UnityCodeSmellAnalyzer\\bin\\Debug\\net472\\results.json");
+            JArray data = JArray.Parse(text);
+            InvocationSmell.FindInvocationSmell(data, new List<string> { "Update", "FixedUpdate" }, new List<string> { "Instantiate", "Destroy" });
+            InvocationSmell.FindInvocationSmell(data, new List<string> { "Update", "FixedUpdate" }, new List<string> { "Find", "FindGameObjectWithTag", "FindGameObjectsWithTag", "FindObjectOfType" });
+        }
+    }
+}
