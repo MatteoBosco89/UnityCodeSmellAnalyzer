71b3153ef4890533b24fba53fe68d941ca1204a4
SenselessMite <pasqualexbob@outlook.com>
2022-10-07 17:42:55 +0200

    Modified console command line for the analyzer

--- Analyzer/UnityDataSmellAnalyzer/Program.cs
+++ Analyzer/UnityDataSmellAnalyzer/Program.cs
@@ -1,225 +1,27 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using CommandLine;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityAnalyzer;
 
 namespace UnityDataSmellAnalyzer
 {
     internal class Program
     {
-        protected static List<string> unityMainFileList = new List<string>();
-        protected static List<string> unityMetaFileList = new List<string>();
-        protected static string[] DIR = {".unity", ".controller", ".prefab", ".mat", ".anim", ".flare", ".assets" };
-        protected static string[] META = { ".meta" };
-        protected static List<UnityData> mainData = new List<UnityData>();
-        protected static List<UnityData> metaData = new List<UnityData>();
-        protected static string RESULT_DIR = "UnityDataResults";
-        protected static string MAIN_FILE = "mainData.json";
-        protected static string META_FILE = "metaData.json";
-        protected static bool found_directory = false;
-        protected static Dictionary<string, string> commands = new Dictionary<string, string>();
-        protected static bool meta = true;
-        static void Main()
+        public static void Main(string[] args)
         {
-            PrintLogo();
-            LoadCommands();
-            string directory = "";
-            List<string> args = Environment.GetCommandLineArgs().ToList();
-            // mandatory -d option
-            if (!args.Contains("-dir"))
-            {
-                Console.WriteLine("-dir is mandatory");
-                ShowHelp();
-                return;
-            }
-
-            if (args.Contains("-help"))
-            {
-                ShowHelp();
-            }
-            if (args.Contains("-dir"))
-            {
-                int i = args.IndexOf("-dir");
-                directory = args[i + 1];
-            }
-            if (args.Contains("-ext_f"))
-            {
-                int i = args.IndexOf("-ext_f");
-                string file_extensions = args[i + 1];
-                LoadExtensionsFile(file_extensions);
-            }
-            if (args.Contains("-ext"))
-            {
-                int i = args.IndexOf("-ext");
-                LoadExtensions(args[i + 1]);
-            }
-            if (args.Contains("-no_meta"))
-            {
-                meta = false;
-            }
-
-            LoadFileList(directory);
-            if (found_directory)
-            {
-                Analyze();
-                SaveResults();
-            }
-            else Console.WriteLine("Error directory " + directory + "Not found");
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o => UnityDataExtractor.Init(o));
+            UnityDataExtractor.Analyze();
         }
-        protected static void LoadFileList(string directory)
-        {
-            Console.WriteLine("Loading Unity File...");
-            try
-            {
-                Console.Write("\tSearching main file...");
-                List<string> files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(f => DIR.Any(f.ToLower().EndsWith)).ToList();
-                foreach (string f in files)
-                { 
-                    unityMainFileList.Add(f);
-                }
-                Console.WriteLine("Done!");
-                Console.Write("\tLoading meta file...");
-                files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(f => META.Any(f.ToLower().EndsWith)).ToList();
-                foreach (string f in files)
-                {
-                    unityMetaFileList.Add(f);
-                }
-                Console.WriteLine("Done!");
-                found_directory = true;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Directory not found!");
-                found_directory = false;
-            }
-            Console.WriteLine("Done!");
-        }
-
-        protected static void Analyze()
-        {
-            Console.WriteLine("Analyzing...");
-            //load all prefab/unity/controller/mat files
-            //load all metafile
-            Console.Write("\tAnalyzig main files...");
-            foreach (string file in unityMainFileList)
-            {
-                if (unityMetaFileList.Contains(file + ".meta"))
-                {
-                    //Console.WriteLine(file);
-                    UnityData d = new UnityData(file, file + ".meta");
-                    unityMetaFileList.Remove(file + ".meta");
-                    mainData.Add(d);
-                }
-            }
-            Console.WriteLine("Done!");
-            if (meta)
-            {
-                Console.Write("\tAnalyzig meta files...");
-                foreach (string file in unityMetaFileList)
-                {
-                    UnityData d = new UnityData(file);
-                    metaData.Add(d);
-                }
-                Console.WriteLine("Done!");
-            }
-            Console.WriteLine("Done!");
-        }
-
-        public static void SaveResults()
-        {
-            Console.WriteLine("Saving Results...");
-            JArray ja = new JArray();
-            foreach (UnityData d in mainData) ja.Add(d.ToJsonObject());
-            Directory.CreateDirectory(RESULT_DIR);
-            File.WriteAllText(RESULT_DIR + "\\" + MAIN_FILE, ja.ToString());
-            Console.WriteLine("\tMain Results saved in " + Path.GetFullPath(MAIN_FILE));
-            ja = new JArray();
-            if (meta)
-            {
-                foreach (UnityData d in metaData) ja.Add(d.ToJsonObject());
-                Directory.CreateDirectory(RESULT_DIR);
-                File.WriteAllText(RESULT_DIR + "\\" + META_FILE, ja.ToString());
-                Console.WriteLine("\tMeta Results saved in " + Path.GetFullPath(META_FILE));
-            }
-            Console.WriteLine("Done\n");
-            
-        }
-        protected static void LoadCommands()
-        {
-            commands.Add("-dir", "Assets directory (use \"path/to/directory\" if there's spaces in the path)");
-            commands.Add("-ext_f", "Extensions list (use \"path/to/directory\" if there's spaces in the path)");
-            commands.Add("-ext", "list of file to read (use \"<extension1>-<extension2>-....\" to insert the extensions to read");
-            commands.Add("-no_meta", "doesn't analyze .meta file");
-            commands.Add("-help", "Shows this message");
-        }
-
-        protected static void LoadExtensionsFile(string file_extensions)
-        {
-            try
-            {
-                string[] lines = File.ReadAllLines(file_extensions);
-                List<string> extensions = new List<string>();
-                foreach(string line in lines)
-                {
-                    string s = line;
-                    if (s[0] != '.') s = '.' + s;
-                    extensions.Add(s);
-                }
-                DIR = extensions.ToArray();
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Extensions file not found, using default extensions");
-            }
-        }
-
-        protected static void LoadExtensions(string line)
-        {
-            string[] exts = line.Split('-');
-            List<string> extensions = new List<string>();
-            if(exts.Length <= 0)
-            {
-                Console.WriteLine("No extensions passed, using default extensions");
-                return;
-            }
-            for(int i = 0; i < exts.Length; i ++)
-            {
-                string s = exts[i];
-                if (s[0] != '.') s = '.' + s;
-                extensions.Add(s);
-            }
-            DIR = extensions.ToArray();
-            Console.WriteLine(DIR[0]);
-
-        }
-        protected static void ShowHelp()
-        {
-            Console.WriteLine(string.Format("{0,-30} {1,-30}", "OPTION", "FUNCTION"));
-            foreach (KeyValuePair<string, string> kv in commands) Console.WriteLine(string.Format("{0,-30} {1,-30}", kv.Key, kv.Value));
-        }
-
-
-        public static void PrintLogo()
-        {
-            string logo = " ____ ___      .__  __                                   \r\n" +
-                    "|    |   \\____ |__|/  |_ ___.__.                         \r\n" +
-                    "|    |   /    \\|  \\   __<   |  |                         \r\n" +
-                    "|    |  /   |  \\  ||  |  \\___  |                         \r\n" +
-                    "|______/|___|  /__||__|  / ____|                         \r\n" +
-                    "             \\/          \\/                              \r\n" +
-                    "                   .____    .__        __                \r\n" +
-                    "                   |    |   |__| _____/  |_  ___________ \r\n" +
-                    "                   |    |   |  |/    \\   __\\/ __ \\_  __ \\\r\n" +
-                    "                   |    |___|  |   |  \\  | \\  ___/|  | \\/\r\n" +
-                    "                   |_______ \\__|___|  /__|  \\___  >__|   \r\n" +
-                    "                           \\/       \\/          \\/       ";
-            Console.WriteLine(logo);
-            Console.WriteLine("\nBy Matteo Bosco, Pasquale Cavoto, Augusto Ungolo\n");
-            Console.WriteLine("Unity meta-data file analyzer for project smell analysis\n");
-        }
+    }
+    public class Options
+    {
+        [Option('a', "assets",Required = true, HelpText = "Path to Assets directory")]
+        public string DataPath { get; set; }
+        [Option('n', "nometa", Required = false, HelpText = "Don't load .meta files")]
+        public bool NoMeta { get; set; }
+        [Option('f', "fileExt", Required = false, HelpText = "Extension.txt file")]
+        public string ExtensionFile { get; set; }
+        [Option('e', "ext", Required = false, HelpText = "List of extension to search")]
+        public IEnumerable<string> Extensions { get; set; }
+        [Option('l', "log", Required = false, HelpText = "Log Level: Trace 0 Debug 1 Information 2 Warning 3 Error 4 Critical 5 None 6 (Debug is Default)")]
+        public int Logging { get; set; }
     }
 }
---
+++ Analyzer/UnityDataSmellAnalyzer/UnityDataExtractor.cs
@@ -0,0 +1,153 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityAnalyzer;
+
+namespace UnityDataSmellAnalyzer
+{
+    public class UnityDataExtractor
+    {
+        protected static List<string> unityMainFileList = new List<string>();
+        protected static List<string> unityMetaFileList = new List<string>();
+        protected static string[] DIR = { ".unity", ".controller", ".prefab", ".mat", ".anim", ".flare", ".assets" };
+        protected static string[] META = { ".meta" };
+        protected static List<UnityData> mainData = new List<UnityData>();
+        protected static List<UnityData> metaData = new List<UnityData>();
+        protected static string RESULT_DIR = "UnityDataResults";
+        protected static string MAIN_FILE = "mainData.json";
+        protected static string META_FILE = "metaData.json";
+        protected static bool meta = true;
+        protected static string directory = null;
+        protected static string file_extensions = null;
+        protected static int logLevel = 1;
+
+        public static void Init(Options opt)
+        {
+            if(opt.DataPath != null)directory = opt.DataPath;
+            if(opt.NoMeta) meta = false;
+            if(opt.Extensions.Count() > 0) LoadExtensions(opt.Extensions);
+            if(opt.ExtensionFile != null) file_extensions = opt.ExtensionFile;
+        }
+        protected static bool LoadFileList()
+        {
+            Console.WriteLine("Loading Unity File...");
+            try
+            {
+                Console.Write("\tSearching main file...");
+                List<string> files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(f => DIR.Any(f.ToLower().EndsWith)).ToList();
+                foreach (string f in files)
+                {
+                    unityMainFileList.Add(f);
+                }
+                Console.WriteLine("Done!");
+                Console.Write("\tLoading meta file...");
+                files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(f => META.Any(f.ToLower().EndsWith)).ToList();
+                foreach (string f in files)
+                {
+                    unityMetaFileList.Add(f);
+                }
+                Console.WriteLine("Done!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Directory not found!");
+                return false;
+            }
+            Console.WriteLine("Done!");
+            return true;
+        }
+
+        public static void Analyze()
+        {
+            if(!LoadFileList())return;
+            if(file_extensions != null)
+            {
+                if (!LoadExtensionsFile()) return;
+            }
+            Console.WriteLine("Analyzing...");
+            //load all prefab/unity/controller/mat files
+            //load all metafile
+            Console.Write("\tAnalyzig main files...");
+            foreach (string file in unityMainFileList)
+            {
+                if (unityMetaFileList.Contains(file + ".meta"))
+                {
+                    //Console.WriteLine(file);
+                    UnityData d = new UnityData(file, file + ".meta");
+                    unityMetaFileList.Remove(file + ".meta");
+                    mainData.Add(d);
+                }
+            }
+            Console.WriteLine("Done!");
+            if (meta)
+            {
+                Console.Write("\tAnalyzig meta files...");
+                foreach (string file in unityMetaFileList)
+                {
+                    UnityData d = new UnityData(file);
+                    metaData.Add(d);
+                }
+                Console.WriteLine("Done!");
+            }
+            Console.WriteLine("Done!");
+            SaveResults();
+        }
+
+        public static void SaveResults()
+        {
+            Console.WriteLine("Saving Results...");
+            JArray ja = new JArray();
+            foreach (UnityData d in mainData) ja.Add(d.ToJsonObject());
+            Directory.CreateDirectory(RESULT_DIR);
+            File.WriteAllText(RESULT_DIR + "\\" + MAIN_FILE, ja.ToString());
+            Console.WriteLine("\tMain Results saved in " + Path.GetFullPath(MAIN_FILE));
+            ja = new JArray();
+            if (meta)
+            {
+                foreach (UnityData d in metaData) ja.Add(d.ToJsonObject());
+                Directory.CreateDirectory(RESULT_DIR);
+                File.WriteAllText(RESULT_DIR + "\\" + META_FILE, ja.ToString());
+                Console.WriteLine("\tMeta Results saved in " + Path.GetFullPath(META_FILE));
+            }
+            Console.WriteLine("Done\n");
+
+        }
+
+        protected static bool LoadExtensionsFile()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(file_extensions);
+                List<string> extensions = new List<string>();
+                foreach (string line in lines)
+                {
+                    string s = line;
+                    if (s[0] != '.') s = '.' + s;
+                    extensions.Add(s);
+                }
+                DIR = extensions.ToArray();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Extensions file not found, using default extensions");
+                return false;
+            }
+            return true;
+        }
+
+        protected static void LoadExtensions(IEnumerable<string> elements)
+        {
+            List<string> extensions = new List<string>();
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                string s = elements.ElementAt(i);
+                if (s[0] != '.') s = '.' + s;
+                extensions.Add(s);
+            }
+            DIR = extensions.ToArray();
+            Console.WriteLine(DIR[0]);
+        }
+    }
+}
