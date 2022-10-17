458149777a55c897d1bb204ccec368f5c98a9b71
Matteo Bosco <36387454+MatteoBosco89@users.noreply.github.com>
2022-10-17 16:48:53 +0200

    Delete example directory

--- example/UnityProjectExample/Assets/ExampleScript/TestSolution/State.cs
+++
@@ -1,18 +0,0 @@
-﻿using System;
-
-namespace Test
-{
-    public class State : StateManager
-    {
-        protected int b;
-        [Obsolete] public State(int a, int b) : base(a) => this.b = b;
-
-        public State(int a, int b, int c) : base(a)        {
-            this.b = b;
-        }
-
-        
-
-    }
-}
-
--- example/UnityProjectExample/Assets/Script/SuperClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SuperClass: MonoBehaviour
-{
-    virtual public void Test() { }
-    virtual public void Test2() { }
-}
